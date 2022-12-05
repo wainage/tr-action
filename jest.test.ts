97c68ce066ae26fcbd2b16bf0e7b30e745c8ff68
Wayne Müller <github@wainage.com>
2022-12-05 13:19:37 -0500

    Added Jest testing with TypeScript tranformers

---
+++ jest.test.ts
@@ -0,0 +1,21 @@
+/**
+ * Jest setup test
+ *
+ * @description Basic test to ensure jest + transformers are working
+ *
+ * @group setup
+ * @group unit
+ */
+describe("Jest Setup Test", () => {
+    test("Basic Test", () => {
+        expect(true).toBeTruthy()
+    })
+    test("NODE_ENV is 'test'", () => {
+        expect(process.env.NODE_ENV).toBe("test");
+    });
+    test.todo("Todo Test")
+    test.skip("Skip Test", () => {
+        // This should never run
+        expect(false).toBeTruthy()
+    })
+})
